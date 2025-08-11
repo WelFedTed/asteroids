9cd9b365b54c417f44b7c4d7de5869ff51b233af
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-08-12 07:18:17 +1000

    added frame-rate limit

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
