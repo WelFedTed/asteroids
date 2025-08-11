e1f199bec097b50f4a6fdaa359b4e5e5dfcd6d8d
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-08-12 07:11:22 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()  # Update the display
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
